994c18ad3f6ce8be8d6f99af5183ec627b1372a6
Nikoleta <nikoleta.yankova96@gmail.com>
2017-04-25 22:37:43 +0300

    added tab fragments

---
+++ app/src/main/java/com/example/nikoleta/newsapp/TabFragment.java
@@ -0,0 +1,34 @@
+package com.example.nikoleta.newsapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TabFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+
+    public TabFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_tab, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view_tab_fragment);
+
+        return view;
+    }
+
+}
